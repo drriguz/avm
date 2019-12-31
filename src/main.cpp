1b35e84abf2ea21a30e413d3b842a71a391c4972
Riguz Lee <solee.linux@gmail.com>
2019-12-31 16:08:47 +0800

    added main entry

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include "parser/class_file_parser.h"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+using namespace avm;
+
+void help() {
+	cout << "Usage:\n" << "guava -classpath <classpath> mainClass" << endl;
+}
+int main(int argc, char *argv[]) {
+	if(argc != 4) {
+		help();
+		return 1;
+	}
+	string classpath = argv[2], entry = argv[3];
+
+	return 0;
+}
