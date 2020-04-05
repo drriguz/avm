1ee20fc4dac3aa29bbbbba2d8e76bb4738f5a8a8
Riguz Lee <solee.linux@gmail.com>
2020-04-05 18:05:01 +0800

    added tests

--- tests/src/vm/runtime/test_local_variables.cpp
+++ tests/src/vm/runtime/test_local_variables.cpp
@@ -10,17 +10,42 @@ TEST(LocalVariables, saveAndGetSingleByteValues) {
 	LocalVariables var(5);
 	var.setByte(0, 18);
 	ASSERT_EQ(18, var.getByte(0));
+	var.setByte(0, 20);
+	ASSERT_EQ(20, var.getByte(0));
+	var.setShort(0, 18);
+	ASSERT_EQ(18, var.getShort(0));
+	var.setInt(0, 1234);
+	ASSERT_EQ(1234, var.getInt(0));
+	var.setFloat(0, 1234.5);
+	ASSERT_EQ(1234.5, var.getFloat(0));
 }
 
 TEST(LocalVariables, saveAndGetDoubleByteValues) {
-	
+	LocalVariables var(5);
+	var.setLong(0, 100000010001);
+	var.setDouble(2, 100000000.88);
+	var.setInt(4, 18);
+	ASSERT_EQ(100000010001, var.getLong(0));
+	ASSERT_EQ(100000000.88, var.getDouble(2));
+	ASSERT_EQ(18, var.getInt(4));
 }
 
 TEST(LocalVariables, saveAndGetNegativeByteValues) {
-	
+	LocalVariables var(5);
+	var.setLong(0, -100000010001);
+	var.setDouble(2, -100000000.88);
+	var.setInt(4, -18);
+	ASSERT_EQ(-100000010001, var.getLong(0));
+	ASSERT_EQ(-100000000.88, var.getDouble(2));
+	ASSERT_EQ(-18, var.getInt(4));
 }
 
 
 TEST(LocalVariables, throwExceptionIfOutOfRange) {
+	LocalVariables var(1);
 	
+	EXPECT_THROW(var.setLong(0, -100000010001),
+			LocalVariablesOutOfRangeException);
+	EXPECT_THROW(var.setLong(2, -100000010001),
+			LocalVariablesOutOfRangeException);
 }
